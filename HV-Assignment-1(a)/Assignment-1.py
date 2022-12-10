1a4720a4fbb4ff6a5b60cf1ff978ab5d65526fdd
Pydasaikrishna <saikrishna.pyda9951@gmail.com>
2022-12-10 14:40:34 +0530

    Adding Assignment-1 for Refresher Classes

---
+++ HV-Assignment-1(a)/Assignment-1.py
@@ -0,0 +1,17 @@
+Data = open("Data_1.txt","a")      #creating the file named as "Data_1.txt"
+
+Number_1 = int(input("Enter Number_1: "))
+Number_2 = int(input("Enter Number_2: "))
+Number_3 = int(input("Enter Number_3: "))
+Number_4 = int(input("Enter Number_4: "))
+Number_5 = int(input("Enter Number_5: "))
+
+Total = Number_1 + Number_2 + Number_3 + Number_4 + Number_5     #adding the all positive numbers
+
+if (Number_1 > 0) & (Number_2 > 0) & (Number_3 > 0) & (Number_4 > 0) & (Number_5 > 0):
+    print("The sum of all Positive Numbers: ",Total)
+    print("The sum of all Positive Numbers:",Total,file=Data)
+else:
+    print("Please enter the Positive Numbers")
+    print("Please enter the Positive Numbers",file=Data)
+
